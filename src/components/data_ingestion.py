75f7222529b8d7883675b4c2ca58513cbb61f6bc
Pruthvish-Vyas <162692946+Pruthvish-Vyas@users.noreply.github.com>
2025-05-24 11:44:48 +0530

    Update data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -25,7 +25,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv(r'C:\Users\91635\OneDrive\VS STUDIO\Portfoliio\Kaggle\2025-May\Ga\notebook\data\Ga.csv')
+            df=pd.read_csv('Ga\notebook\data\Ga.csv')
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
